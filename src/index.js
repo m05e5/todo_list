2ed5fd9be29c23172e6d8fcb2f4ec6a7aeafea99
m05e5 <calebmoses1111@gmail.com>
2021-07-13 15:33:02 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import './style.css';
-
+const todo_div = document.querySelector('.todo-list');
 function component() {
   const element = document.createElement('div');
 
@@ -9,5 +9,25 @@ function component() {
 
   return element;
 }
+
+const lunchTodoList = () => {
+  const headSection = document.createElement('div');
+  headSection.classList.add('head-section');
+  const headTitle = document.createElement('p');
+  headTitle.classList.add('head-title');
+  headTitle.innerText = `Today's To Do`;
+  headSection.appendChild(headTitle);
+  const headIcon = document.createElement('img');
+  headIcon.classList.add('head-img');
+  headIcon.src = '../assets/reset.png';
+  headIcon.alt = 'reload';
+  headIcon.id = 'headIcon';
+  headSection.appendChild(headIcon);
+  todo_div.appendChild(headSection);
+
+
+}
+
+lunchTodoList();
 
 document.body.appendChild(component());
